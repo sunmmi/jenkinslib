cd7565bdad6d0337539b7af46821c331bfb13431
sunmmi <sunmmi@sina.com>
2020-08-13 16:34:37 +0800

    Update build.groovy

--- src/org/pipeline/build.groovy
+++ src/org/pipeline/build.groovy
@@ -2,9 +2,10 @@ package org.pipeline
 
 //构建类型
 def Build(buildType,buildShell){
-    def buildTools = ["mvm":"maven","ant":"ant","gradle":"gradle","npm":"npm"]
-        println("当前选择的构建类型为：${buildType}")
+    def buildTools = ["mvn":"maven","ant":"ant","gradle":"gradle","npm":"npm"]
+    
+    println("当前选择的构建类型为：${buildType}")
     buildHome = tool buildTools[buildType]
     
-    sh "${buildHome/bin/${buildType} ${buildShell}"
+    sh "${buildHome}/bin/${buildType} ${buildShell}"
 }
