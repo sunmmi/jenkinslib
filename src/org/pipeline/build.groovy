c4d57e55900e3a4396eebac13affe8b040d1eaf1
sunmmi <sunmmi@sina.com>
2020-08-13 16:47:36 +0800

    Update build.groovy

--- src/org/pipeline/build.groovy
+++ src/org/pipeline/build.groovy
@@ -7,5 +7,12 @@ def Build(buildType,buildShell){
     println("当前选择的构建类型为：${buildType}")
     buildHome = tool buildTools[buildType]
     
-    sh "${buildHome}/bin/${buildType} ${buildShell}"
+    if ("${buildType}" == "npm"){
+        sh """
+            source /etc/profile
+            ${buildHome}/bin/${buildType} ${buildshell}
+            """
+    } else {
+        sh "${buildHome}/bin/${buildType} ${buildShell}"
+    }
 }
